5d399490cadea6bce66be706c7044350d402248a
Ignacio Villafañe <villafaneigna@gmail.com>
2024-08-04 23:24:14 -0300

    Navigation:Stack, Tab

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { CartProvider } from "./context/CartContext";
 import Navigation from "./Navigation";
@@ -8,15 +7,13 @@ import Navigation from "./Navigation";
 export default function App() {
   return (
     <CartProvider>
-      <NavigationContainer>
-        <SafeAreaView
-          style={styles.container}
-          contentContainerStyle={{ padding: 15 }}
-        >
-          <Navigation />
-          <StatusBar style="dark" backgroundColor="white" />
-        </SafeAreaView>
-      </NavigationContainer>
+      <SafeAreaView
+        style={styles.container}
+        contentContainerStyle={{ padding: 15 }}
+      >
+        <Navigation />
+        <StatusBar style="dark" backgroundColor="white" />
+      </SafeAreaView>
     </CartProvider>
   );
 }
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,55 +1,59 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import BtnPrimary from './addons/btnPrimary'
-import BtnSecondary from './addons/btnSecondary'
-import BtnOptional from './addons/btnOptional'
-import BtnWhite from './addons/btnWhite'
-import coffeBeans from "../assets/coffebeans.png"
-import logoGoogle from "../assets/google.webp"
-import { useNavigation } from '@react-navigation/native';
-
+import { StyleSheet, Text, View, Image } from "react-native";
+import BtnPrimary from "./addons/btnPrimary";
+import BtnSecondary from "./addons/btnSecondary";
+import BtnOptional from "./addons/btnOptional";
+import BtnWhite from "./addons/btnWhite";
+import coffeBeans from "../assets/coffebeans.png";
+import logoGoogle from "../assets/google.webp";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Home() {
-
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>Bienvenidos a Coffee Store</Text>
-        <Image source={coffeBeans} style={styles.image}></Image>
-        <View style={styles.subcontainer}>
-        <BtnPrimary title="Ingresar" sizeBtn={150} onPress={() => navigation.navigate("Login")} />
-        <BtnSecondary title="Registrarse" sizeBtn={150} onPress={() => navigation.navigate("Register")} />
-        <BtnWhite title="Continuar con Google" imageSrc={logoGoogle} onPress={()=> navigation.navigate("Login")}/>
-        </View>
-        
+      <Text style={styles.title}>Bienvenidos a Coffee Store</Text>
+      <Image source={coffeBeans} style={styles.image}></Image>
+      <View style={styles.subcontainer}>
+        <BtnPrimary
+          title="Ingresar"
+          sizeBtn={150}
+          onPress={() => navigation.navigate("Login")}
+        />
+        <BtnSecondary
+          title="Registrarse"
+          sizeBtn={150}
+          onPress={() => navigation.navigate("Register")}
+        />
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#EDF1D6",
-    },
-    title: {
-        fontSize: 40,
-        textAlign: "center",
-        fontWeight: "bold",
-    },
-    image: {
-        width: 200,
-        height: 200,
-        resizeMode: "contain"
-    },
-    subcontainer: {
-        width: "100%",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        gap: 15,
-        marginTop: 20
-    }
-})
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#EDF1D6",
+  },
+  title: {
+    fontSize: 40,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+  subcontainer: {
+    width: "100%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: 15,
+    marginTop: 20,
+  },
+});
